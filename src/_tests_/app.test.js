f60da470a3c404e133175c9f8c7a8ffe44f1d061
Nathan Loveless <nathansl2003@yahoo.com>
2020-04-15 21:36:08 -0400

    Completed unit test, FE code template is complete.

--- src/_tests_/app.test.js
+++ src/_tests_/app.test.js
@@ -2,9 +2,16 @@ import React from 'react';
 import * as rtl from '@testing-library/react';
 import '@testing-library/jest-dom';
 import App from '../App';
+import { queryByText } from '@testing-library/react';
+
+it('It loads initial react app entry point', () => {
+    rtl.render(<App />
+        );
+});
 
 it('renders "You have rendered properly"', () => {
-    const wrapper = rtl.render(<App />
-        );
-    console.log(wrapper.debug());
-});
+
+    const element = rtl.render(<App />).queryByText(/You have rendered properly!/i);
+    expect(element).toBeInTheDocument();
+
+})
