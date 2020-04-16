ac57527eb05c92d1fde22e2830f9d6d792d01eeb
Nathan Loveless <nathansl2003@yahoo.com>
2020-04-15 20:32:37 -0400

    Added template files, installed jest

---
+++ src/app.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+//import App from "./components/App";
+
+function App() {
+  return <div className="App">You have rendered properly!</div>;
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
