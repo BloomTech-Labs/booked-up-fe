c9604d1388d4068acdd9f484b7e05ba39d3663df
Nathan Loveless <nathansl2003@yahoo.com>
2020-04-21 20:23:38 -0400

    re-setup jest, configured eslint, prettier, dotenv changed for AWS

--- _tests_/app.test.js
+++ _tests_/app.test.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import * as rtl from '@testing-library/react';
-import '@testing-library/jest-dom';
-import App from '../src/App'
+import React from "react";
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom";
+import App from "../src/App";
 
-it('It loads initial react app entry point', () => {
-    rtl.render(<App />
-        );
+it("It loads initial react app entry point", () => {
+  rtl.render(<App />);
 });
 
-it('renders "You have rendered properly"', () => {
-
-    const element = rtl.render(<App />).queryByText(/You have rendered properly!/i);
-    expect(element).toBeInTheDocument();
-})
+it('renders "Hi, you are rendering!"', () => {
+  const element = rtl.render(<App />).queryByText(/Hi, you are rendering!/i);
+  expect(element).toBeInTheDocument();
+});
