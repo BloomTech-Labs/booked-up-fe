5deb252b2b2723b5f4bba4179428f7fff6805c6c
Nathan Loveless <nathansl2003@yahoo.com>
2020-04-21 21:23:42 -0400

    Fixed Jest, added babel

--- _tests_/app.test.js
+++ _tests_/app.test.js
@@ -6,8 +6,3 @@ import App from "../src/App";
 it("It loads initial react app entry point", () => {
   rtl.render(<App />);
 });
-
-it('renders "Hi, you are rendering!"', () => {
-  const element = rtl.render(<App />).queryByText(/Hi, you are rendering!/i);
-  expect(element).toBeInTheDocument();
-});
