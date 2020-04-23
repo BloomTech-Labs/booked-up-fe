e5b8a47af8d6bee608f978a9c4cc9b9b53457d23
Derek <legato_5@yahoo.com>
2020-04-22 21:06:14 -0700

    set up privaterouter for a simple authentication check. basic structure of router built out.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ThemeProvider } from "@material-ui/styles";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import PrivateRoute from "./utils/PrivateRoute.jsx";
+import theme from "./utils/Theme"
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
   return (
-    <ThemeProvider>
+    <ThemeProvider theme={theme}>
       <BrowserRouter>
         <Switch>
+          <PrivateRoute exact path="/dashboard" component={() => <div>Dashboard</div>} isAuthenticated={isAuthenticated}/>
           <Route exact path="/login" component={() => <div>Log In</div>} />
-          <Route exact path="/" component={() => <div>Home</div>} />
-          <PrivateRoute path="/dashboard" component={() => <div>Dashboard</div>} />
+          <Route exact path="/" component={() => <div>Home</div>} /> 
           <Route exact path="/signup" component={() => <div>Sign Up</div>} />
         </Switch>
       </BrowserRouter>
--- src/utils/PrivateRoute.jsx
+++ src/utils/PrivateRoute.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-const PrivateRoute = ({component: Component, ...otherProps}) => {
+const PrivateRoute = ({component: Component, isAuthenticated, ...otherProps}) => {
     return (
         <>
             <Route {...otherProps}
-            render={() => {
-                if(localStorage.getItem('token')) {
+            render={(props) => {
+                // if(localStorage.getItem('token')) {
+                if(isAuthenticated) {
                     return <Component />;
                 } 
                 else {
-                    return <Redirect to='login' />
+                    return <Redirect to='/login' />
                 }
                 }
             } />
