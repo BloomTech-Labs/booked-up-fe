9d15b5f134abe52c459949bb7a58a13c14a3c8ea
Derek <legato_5@yahoo.com>
2020-04-22 20:15:11 -0700

    started theme for material-ui and initialized routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { ThemeProvider } from "@material-ui/styles";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-        <p>Hi, you are rendering!</p>
-    </div>
+    <ThemeProvider>
+      <BrowserRouter>
+        <Switch>
+          
+        </Switch>
+          <div className="App">
+            <p>Hi, you are rendering!</p>
+          </div>
+      </BrowserRouter>
+    </ThemeProvider>
+    
   );
 }
 
