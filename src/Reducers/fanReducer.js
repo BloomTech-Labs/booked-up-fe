8ed3805fb235f5ce081637cb4099bd096b68c2e5
Nathan Loveless <nathansl2003@yahoo.com>
2020-04-22 23:31:32 -0400

    Created reducer files and action files, stubbed out reducerfiles

---
+++ src/Reducers/fanReducer.js
@@ -0,0 +1,10 @@
+export const fanReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "Something": {
+      return state;
+    }
+    default: {
+      return state;
+    }
+  }
+};
