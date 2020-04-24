a11f515f01194a49175ffce92622c1948bc00d0b
Shafi Masoumi <masoumi2017@icould.com>
2020-04-23 19:26:01 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Router, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Login from './SharedComponents/login';
 
 function App() {
   return (
     <div className="App">
         <Switch>
-          <Route path="login" component={Login} />    
+          <Route path="login" component={Login}/>    
         </Switch>
     </div>
   );
