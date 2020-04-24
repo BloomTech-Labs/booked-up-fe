164fb12fb6fbcb82d77f319317fcfaa6b12d4b68
Derek Glynn <48306976+wolfwood87@users.noreply.github.com>
2020-04-23 19:23:41 -0700

    Routing setup (#11)
    * started theme for material-ui and initialized routes
    
    * added PrivateRoute component
    
    * added book icon
    
    * set up privaterouter for a simple authentication check. basic structure of router built out.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from "react";
+
+import React, { useState } from 'react';
+import { ThemeProvider } from "@material-ui/styles";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import PrivateRoute from "./utils/PrivateRoute.jsx";
+import theme from "./utils/Theme"
+
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
   return (
-    <div className="App">
-      <p>Hi, you are rendering!</p>
-    </div>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Switch>
+          <PrivateRoute exact path="/dashboard" component={() => <div>Dashboard</div>} isAuthenticated={isAuthenticated}/>
+          <Route exact path="/login" component={() => <div>Log In</div>} />
+          <Route exact path="/" component={() => <div>Home</div>} /> 
+          <Route exact path="/signup" component={() => <div>Sign Up</div>} />
+        </Switch>
+      </BrowserRouter>
+    </ThemeProvider>
+
   );
 }
 
