eca6ba226e953c6240e9e93ace3902c15dfe67a8
Derek Glynn <48306976+wolfwood87@users.noreply.github.com>
2020-04-27 18:46:32 -0700

    Create dashboard (#13)
    * created header and footer using material-ui. Need to finish up footer.
    
    * finished footer setup.
    
    * created basic header and footer. Ready to move on to next part.
    
    * created dashboard component.
    
    * changes to header logo

--- src/utils/Theme.js
+++ src/utils/Theme.js
@@ -1,5 +1,17 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 
+const primary = "#C4C4C4";
+const secondary = "#3F47CC";
+
 export default createMuiTheme({
-    
+    palette: {
+        
+        primary: {
+          main: `${primary}`
+        },
+        secondary: {
+          main: `${secondary}`
+        },
+    }
+      
 })
