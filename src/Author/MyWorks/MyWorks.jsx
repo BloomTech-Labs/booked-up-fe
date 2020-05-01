e541d812465fc168c0cbc572ce9ddd7ed4032e35
Nathan Loveless <51862134+nathan-loveless@users.noreply.github.com>
2020-04-30 23:42:44 -0400

    Nl my works feature (#16)
    * Setup MyWorks components, starting styling MyWorks container
    
    * Completed 2 of 3 views, completed interface design for content
    
    Co-authored-by: Nathan Loveless <nathansl2003@yahoo.com>
    Co-authored-by: Derek Glynn <48306976+wolfwood87@users.noreply.github.com>

---
+++ src/Author/MyWorks/MyWorks.jsx
@@ -0,0 +1,132 @@
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import ToggleButton from "@material-ui/lab/ToggleButton";
+import ToggleButtonGroup from "@material-ui/lab/ToggleButtonGroup";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import TextField from "@material-ui/core/TextField";
+import IconButton from "@material-ui/core/IconButton";
+import GridDisplay from "./GridDisplay";
+import RowDisplay from "./RowDisplay";
+import ColumnDisplay from "./ColumnDisplay";
+import { data } from "../../data";
+import ViewModuleOutlinedIcon from "@material-ui/icons/ViewModuleOutlined";
+import ViewStreamOutlinedIcon from "@material-ui/icons/ViewStreamOutlined";
+import ViewWeekOutlinedIcon from "@material-ui/icons/ViewWeekOutlined";
+import PublishOutlinedIcon from "@material-ui/icons/PublishOutlined";
+import DeleteForeverOutlinedIcon from "@material-ui/icons/DeleteForeverOutlined";
+import EditOutlinedIcon from "@material-ui/icons/EditOutlined";
+import OpenInBrowserOutlinedIcon from "@material-ui/icons/OpenInBrowserOutlined";
+import SortOutlinedIcon from "@material-ui/icons/SortOutlined";
+import FilterListOutlinedIcon from "@material-ui/icons/FilterListOutlined";
+import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
+
+const useStyles = makeStyles(theme => ({
+  toolbar: {
+    display: "flex",
+    backgroundColor: theme.palette.secondary.light,
+    margin: theme.spacing(2, 0),
+    marginLeft: "2px",
+    marginRight: "2px",
+    marginTop: "2px",
+    marginBottom: "2px",
+    border: `2px solid ${theme.palette.secondary.dark}`,
+    borderRadius: "10px"
+  },
+  leftToolbarButton: {
+    marginRight: "25px"
+  },
+  rightToolbarButtonGroup: {
+    backgroundColor: theme.palette.secondary.light,
+    marginRight: "25px",
+    paddingTop: "2px"
+  },
+  rightButtonGroup: {
+    backgroundColor: theme.palette.secondary.light,
+    marginRight: "25px"
+  },
+  searchGroup: {
+    display: "flex",
+    alignItems: "center",
+    textAlign: "middle",
+    marginLeft: "100px"
+  },
+  searchBar: {
+    marginLeft: "10px",
+    marginBottom: "5px"
+  },
+  contentArea: {
+    marginTop: "35px",
+    marginLeft: "35px",
+    marginRight: "35px"
+  }
+}));
+
+const userData = data;
+
+export default function MyWorks(props) {
+  const [selected, setSelected] = useState("grid");
+
+  const handleSelect = (event, selected) => {
+    setSelected(selected);
+  };
+  const classes = useStyles();
+  return (
+    <>
+      <div className={classes.toolbar}>
+        <div className={classes.leftToolbarButton}>
+          <IconButton>
+            <PublishOutlinedIcon />
+          </IconButton>
+        </div>
+        <div className={classes.rightToolbarButtonGroup}>
+          <ToggleButtonGroup
+            value={selected}
+            exclusive
+            className={classes.rightButtonGroup}
+            onChange={handleSelect}
+            aria-label="Text Alignment"
+          >
+            <ToggleButton
+              value="grid"
+              className={classes.iconButton}
+              aria-label="grid"
+            >
+              <ViewModuleOutlinedIcon />
+            </ToggleButton>
+            <ToggleButton
+              value="row"
+              className={classes.iconButton}
+              aria-label="row"
+            >
+              <ViewWeekOutlinedIcon />
+            </ToggleButton>
+            <ToggleButton
+              value="column"
+              className={classes.iconButton}
+              aria-label="column"
+            >
+              <ViewStreamOutlinedIcon />
+            </ToggleButton>
+          </ToggleButtonGroup>
+        </div>
+        <ButtonGroup>
+          <IconButton>
+            <SortOutlinedIcon />
+          </IconButton>
+          <IconButton>
+            <FilterListOutlinedIcon />
+          </IconButton>
+        </ButtonGroup>
+        <div className={classes.searchGroup}>
+          <SearchOutlinedIcon />
+          <TextField id="search" className={classes.searchBar} label="Search" />
+        </div>
+      </div>
+      <div className={classes.contentArea}>
+        {selected === "grid" && <GridDisplay authorWorks={data.author_works} />}
+        {selected === "row" && <RowDisplay authorWorks={data.author_works} />}
+        {selected === "column" && <ColumnDisplay />}
+      </div>
+    </>
+  );
+}
--- src/SharedComponents/Footer.jsx
+++ src/SharedComponents/Footer.jsx
@@ -1,76 +1,84 @@
-import React, { useState, useEffect } from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import useScrollTrigger from '@material-ui/core/useScrollTrigger';
-import { makeStyles } from '@material-ui/styles';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
+import React, { useState, useEffect } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import { makeStyles } from "@material-ui/styles";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
 import Button from "@material-ui/core/Button";
-import Typography from '@material-ui/core/Typography';
+import Typography from "@material-ui/core/Typography";
 import { Link } from "react-router-dom";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
-import { BottomNavigation, BottomNavigationAction } from '@material-ui/core';
-
+import { BottomNavigation, BottomNavigationAction } from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
-    toolbarMargin: {
+  toolbarMargin: {
         marginBottom: "4em"
-      },
-    appBar: {
-        top: 'auto',
-        bottom: 0,
-      },
-    toolBar: {
-        backgroundColor: theme.palette.secondary.dark,
-        display: "flex",
-        justifyContent: "center",
-        flexDirection: "column",
-        alignItems: "center"
-    },
-    navContainer: {
-        backgroundColor: theme.palette.secondary.dark,
-        height: "1%",
-        width: "50%",
-        display: "flex",
-        alignItems: "flex-start",
-
-    },
-    nav: {
-        color: "white",
-        marginLeft: "1%",
-        marginRight: "1%",
-        marginTop: "1%",
-    },
-    div: {
-        color: "white",
-        marginLeft: "1%",
-        marginRight: "1%",
-        marginTop: "1.5%",
-    },
-}))
+  },
+  appBar: {
+    top: "auto",
+    bottom: 0
+  },
+  toolBar: {
+    backgroundColor: theme.palette.secondary.dark,
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  navContainer: {
+    backgroundColor: theme.palette.secondary.dark,
+    height: "1%",
+    width: "50%",
+    display: "flex",
+    alignItems: "flex-start"
+  },
+  nav: {
+    color: "white",
+    marginLeft: "1%",
+    marginRight: "1%",
+    marginTop: "1%"
+  },
+  div: {
+    color: "white",
+    marginLeft: "1%",
+    marginRight: "1%",
+    marginTop: "1.5%"
+  }
+}));
 
 export default function Footer(props) {
-    const classes = useStyles();
-    return (
-        <>
-            <div className={classes.toolbarMargin}></div>
-            <AppBar position="fixed" className={classes.appBar}>
-            <Toolbar className={classes.toolBar}>
-                <BottomNavigation showLabels className={classes.navContainer}>
-                    <BottomNavigationAction label="Browse"  className={classes.nav}/>
-                    <BottomNavigationAction label="Support"  className={classes.nav}/>
-                    <BottomNavigationAction label="Contact Us"  className={classes.nav}/>
-                </BottomNavigation>
-                <BottomNavigation showLabels className={classes.navContainer}>
-                    <BottomNavigationAction label="@2020 Booked Up"  className={classes.nav}/>
-                    <p className={classes.div}>|</p>
-                    <BottomNavigationAction label="Terms of Service"  className={classes.nav}/>
-                    <p className={classes.div}>|</p>
-                    <BottomNavigationAction label="Privacy Policy"  className={classes.nav}/>
-                </BottomNavigation>
-            </Toolbar>
-            </AppBar>
-        </>
-    )
+  const classes = useStyles();
+  return (
+    <>
+    <div className={classes.toolbarMargin}></div>
+      <AppBar className={classes.appBar}>
+        <Toolbar className={classes.toolBar}>
+          <BottomNavigation showLabels className={classes.navContainer}>
+            <BottomNavigationAction label="Browse" className={classes.nav} />
+            <BottomNavigationAction label="Support" className={classes.nav} />
+            <BottomNavigationAction label="Contact Us" className={classes.nav} />
+          </BottomNavigation>
+          <BottomNavigation showLabels className={classes.navContainer}>
+            <BottomNavigationAction
+              label="@2020 Booked Up"
+              className={classes.nav}
+            />
+            <p className={classes.div}>|</p>
+            <BottomNavigationAction
+              label="Terms of Service"
+              className={classes.nav}
+            />
+            <p className={classes.div}>|</p>
+            <BottomNavigationAction
+              label="Privacy Policy"
+              className={classes.nav}
+            />
+          </BottomNavigation>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
 }
+
