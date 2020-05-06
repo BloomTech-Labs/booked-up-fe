f0c8253302ab2e8d6e5450b0a053f42b8fa8f814
Shafi Masoumi <masoumi2017@icould.com>
2020-05-06 14:14:50 -0700

    updates and styles for SignIn/Up and logo

--- src/SharedComponents/SingupForm.jsx
+++ src/SharedComponents/SingupForm.jsx
@@ -1,125 +1,185 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React, { useState } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import Paper from '@material-ui/core/Paper';
 
-//import { userActions } from '../_actions';
 
-class RegisterPage extends React.Component {
-    constructor(props) {
-        super(props);
+const useStyles = makeStyles((theme) => ({
+    root: {
+      height: '100vh',
+    },
+    side: {
+      backgroundColor: 'GhostWhite',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
 
-        this.state = {
-            user: {
-                userType: '',
-                firstName: '',
-                lastName: '',
-                username: '',
-                emailAddress: '',
-                password: '',
-                confirmPassword: ''
-            },
-            submitted: false
-        };
+    },
+    paper: {
+      margin: theme.spacing(8, 4),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+    formControl: {
+        margin: theme.spacing(2, 0, 2, 0),
+        minWidth: '97%',
+      },
+    selectEmpty: {
+        marginTop: theme.spacing(12),
+    },
+  }));
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+ 
+export default function SignUp(props) {
+    const classes = useStyles();
+    const [creds, setCreds] = useState({})
+    const [userType, setUserType] = useState('');
+
+    const onSubmit = e => {
+        e.preventDefault()
+        props.signup(creds, props.history)
     }
 
-    handleChange(event) {
-        const { name, value } = event.target;
-        const { user } = this.state;
-        this.setState({
-            user: {
-                ...user,
-                [name]: value
-            }
-        });
-    }
+    const handleChangeUserType = (event) => {
+        setUserType(event.target.value);
+    };
 
-    handleSubmit(event) {
-        event.preventDefault();
+    return (
+        <Grid container component="main" className={classes.root}>
+            <CssBaseline />
+            <Grid item xs={false} sm={4} md={7} className={classes.side} />
+            <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                <div className={classes.paper} component="main" maxWidth="xs">
+                    <Avatar className={classes.avatar}>
+                        <AccountCircleIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign up
+                    </Typography>
+                    <form className={classes.form} noValidate onSubmit={onSubmit}>
+                        <Grid item xs={6}>
+                            <FormControl variant="outlined" className={classes.formControl}>
+                                <InputLabel id="demo-simple-select-outlined-label">User Type</InputLabel>
+                                <Select
+                                    labelId="demo-simple-select-outlined-label"
+                                    id="demo-simple-select-outlined"
+                                    value={userType}
+                                    onChange={handleChangeUserType}
+                                    label="User type"
+                                    color="secondary"
+                                >
+                                    <MenuItem value={"agent"}>Agent</MenuItem>
+                                    <MenuItem value={"author"}>Author</MenuItem>
+                                    <MenuItem value={"fan"}>Fan</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </Grid>
 
-        this.setState({ submitted: true });
-        const { user } = this.state;
-        if (user.userType && user.firstName && user.lastName && user.username && user.emailAddress && user.password && user.confirmPassword) {
-            this.props.register(user);
-        }
-    }
-
-
-    render() {
-        const { registering  } = this.props;
-        const { user, submitted } = this.state;
-        return (
-            <div className="col-md-6 col-md-offset-3">
-                <h2>Sign Up</h2>
-                <form name="form" onSubmit={this.handleSubmit}>
-              <div className={'form-group' + (submitted && !user.userType ? ' has-error' : '')}>
-
-                <select  type="text" className="form-control" name="userType" value={user.userType} onChange={this.handleChange} > 
-                <option value="">- - - Please Select User Type  - - - </option > 
-                <option value="Agent">Agent</option>
-                   <option value="Author">Author</option>
-                   <option value="Fan">Fan</option>
-                </select>
-                {submitted && !user.userType&&
-                  <div className="help-block">PLease Select User Type</div>
-                }
-              </div>
-                    <div className={'form-group' + (submitted && !user.firstName ? ' has-error' : '')}>
-                        <input placeholder='First Name' type="text" className="form-control" name="firstName" value={user.firstName} onChange={this.handleChange} />
-                        {submitted && !user.firstName &&
-                            <div className="help-block">First Name is required</div>
-                        }
-                    </div>
-                    <div className={'form-group' + (submitted && !user.lastName ? ' has-error' : '')}>
-                        <input placeholder='Last Name' type="text" className="form-control" name="lastName" value={user.lastName} onChange={this.handleChange} />
-                        {submitted && !user.lastName &&
-                            <div className="help-block">Last Name is required</div>
-                        }
-                    </div>
-                    <div className={'form-group' + (submitted && !user.username ? ' has-error' : '')}>
-                        <input type="text" placeholder='User Name' className="form-control" name="username" value={user.username} onChange={this.handleChange} />
-                        {submitted && !user.username &&
-                            <div className="help-block">Username is required</div>
-                        }
-                    </div>
-              <div className={'form-group' + (submitted && !user.emailAddress ? ' has-error' : '')}>
-                <input type="text" placeholder='Email Address' className="form-control" name="emailAddress" value={user.emailAddress} onChange={this.handleChange} />
-                {submitted && !user.emailAddress &&
-                  <div className="help-block">Email address is required</div>
-                }
-              </div>
-                    <div className={'form-group' + (submitted && !user.password ? ' has-error' : '')}>
-                        <input placeholder='Password' type="password" className="form-control" name="password" value={user.password} onChange={this.handleChange} />
-                        {submitted && !user.password &&
-                            <div className="help-block">Password is required</div>
-                        }
-                    </div>
-              <div className={'form-group' + (submitted && !user.confirmPassword ? ' has-error' : '')}>
-                <input placeholder='Confirm Password' type="confirmPassword" className="form-control" name="confirmPassword" value={user.confirmPassword} onChange={this.handleChange} />
-                {submitted && !user.confirmPassword &&
-                  <div className="help-block">Password confirmation is required</div>
-                }
-                    </div>
-                    <div className="form-group">
-                        <button className="btn btn-primary">Register</button>
-                        <Link to="/login" className="btn btn-link">Already have an account</Link>
-                    </div>
-                </form>
-            </div>
-        );
-    }
+                        <Grid container spacing={2}>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    autoComplete="fname"
+                                    name="firstName"
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    id="firstName"
+                                    label="First Name"
+                                    color="secondary"
+                                    autoFocus
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    id="lastName"
+                                    label="Last Name"
+                                    name="lastName"
+                                    color="secondary"
+                                    autoComplete="lname"
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    id="username"
+                                    label="User Name"
+                                    name="username"
+                                    color="secondary"
+                                    autoComplete="username"
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    id="email"
+                                    label="Email Address"
+                                    name="email"
+                                    color="secondary"
+                                    autoComplete="email"
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    name="password"
+                                    label="Password"
+                                    type="password"
+                                    id="password"
+                                    color="secondary"
+                                    autoComplete="current-password"
+                                />
+                            </Grid>
+                        </Grid>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="secondary"
+                            className={classes.submit}
+                        >
+                            Sign Up
+                        </Button>
+                        <Grid container justify="flex-center">
+                            <Grid item>
+                                <Link href="/login" variant="body2">
+                                    Already have an account? Sign in
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </form>
+                </div>
+            </Grid>
+        </Grid>
+    );
 }
-
-function mapState(state) {
-    const { registering } = state.registration;
-    return { registering };
-}
-
-const actionCreators = {
-    register: userActions.register
-}
-
-const connectedRegisterPage = connect(mapState, actionCreators)(RegisterPage);
-export { connectedRegisterPage as RegisterPage };
--- src/SharedComponents/loginForm.jsx
+++ src/SharedComponents/loginForm.jsx
@@ -1,51 +1,120 @@
-import react, { useState } from "react";
-import { connect } from 'react-redux';
-import { Link, withRouter } from 'react-router-dom';
+import React, { useState } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import Paper from '@material-ui/core/Paper';
 
 
-function Login(props) {
+const useStyles = makeStyles((theme) => ({
+    root: {
+        height: '90vh',
+    },
+    side: {
+        backgroundColor: 'GhostWhite',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+    },
+    paper: {
+        margin: theme.spacing(8, 4),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%',
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
 
-    const [creds, setCreds] = setState({})
 
-    const handleChange = e => {
-        setCreds({
-            ...creds,
-            [e.target.name]: e.target.value
-        })
-    }
+export default function SignIn(props) {
+
+    const [creds, setCreds] = useState({})
+    const classes = useStyles();
+
 
     const onSubmit = e => {
         e.preventDefault()
-        props.login(creds, props.history)
+        props.signin(creds, props.history)
     }
 
     return (
-        <>
-         <div className='form-container' style={{ background: '#dfe6ed', margin: '8rem auto 0', border: '1px solid #cbd4de'}}>
-             <h1>Login</h1>
-             <form onSubmit={onSubmit}>
-              <div className='form-group'>
-                  <input className='form-control focus:border' type='text' name='username' placeholder='username' value={creds.username} onChange={handleChange}></input>
-              </div>
-              <div className='form-group'>
-                 <input className='form-control focus:border' type='text' name='password' placeholder='password' value={creds.password} onChange={handleChange} ></input>
-              </div>
-              <button className="btn btn-primary" type="submit">Login</button>
-             </form>  
-         </div>
-        
-         <div className='form-footer'>
-             <Link to='/registration'>Create an account</Link>
-         </div>
-        </>
-    )
+        <Grid container component="main" className={classes.root}>
+            <CssBaseline />
+            <Grid item xs={false} sm={4} md={7} className={classes.side} />
+            <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                <div boxShadow={5} className={classes.paper}>
+                    <Avatar className={classes.avatar}>
+                        <AccountCircleIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                    <form className={classes.form} noValidate onSubmit={onSubmit}>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="username"
+                            label="User Name"
+                            name="username"
+                            autoComplete="username"
+                            color="secondary"
+                            autoFocus
+                            value={creds.username}
+                        />
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            color="secondary"
+                            autoComplete="current-password"
+                            value={creds.password}
+                        />
+                        <FormControlLabel
+                            control={<Checkbox value="remember" color="secondary" />}
+                            label="Remember me"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="secondary"
+                            className={classes.submit}
+                        >
+                            Sign In
+                        </Button>
+                        <Grid container color='red'>
+                            <Grid item>
+                                <Link href="/signup" variant="body2">
+                                    Don't have an account? Sign Up
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </form>
+                </div>
+            </Grid>
+        </Grid>
+    );
 }
-
-const mapStateToProps = state => ({
-    loginError: state.loginError
-})
-
-export default connect(
-    mapStateToProps,
-    { login }
-)(withRouter(login))
