e8dff2714db973e4c9fcd15d430e55920f78802f
Derek <legato_5@yahoo.com>
2020-06-07 16:46:05 -0700

    removed indexroute because fix is working

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import Dashboard from "./SharedComponents/Dashboard/Dashboard.jsx";
 import Home from "./LandingPage/Home";
 import LoginForm from "./SharedComponents/LoginForm";
 import SignUpForm from "./SharedComponents/SignUpForm";
-import IndexRoute from "./utils/IndexRoute.jsx";
 
 
 function App() {
@@ -22,7 +21,6 @@ function App() {
           <Route exact path="/login/" component={LoginForm} />
           <Route exact path="/" component={Home} />
           <Route exact path="/signup/" component={SignUpForm} />
-          {/* <IndexRoute exact path="/index.html" /> */}
         </Switch>
         <Footer />
       </BrowserRouter>
