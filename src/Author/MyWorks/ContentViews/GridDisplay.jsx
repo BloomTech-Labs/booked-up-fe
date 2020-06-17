4d87a0bce1848e9e40c91009be7ec95c50fbeb71
Derek <legato_5@yahoo.com>
2020-06-17 10:54:26 -0700

    default placeholder image now in place

--- src/Author/MyWorks/ContentViews/GridDisplay.jsx
+++ src/Author/MyWorks/ContentViews/GridDisplay.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import EditingButtons from "../EditingButtons";
+import ImagePlaceholder from "../../../assets/image-placeholder.png";
 
 const useStyles = makeStyles(theme => ({
   grid: {
@@ -50,11 +51,24 @@ export default function GridDisplay(props) {
   useEffect(() => {
     console.log(props.authorWorks);
   }, [props.authorWorks]);
+
+  const imageSet = (work) => {
+    if(work.img_url) {
+      return {
+        backgroundImage: `url("${work.img_url}")`
+      }
+    }
+    else {
+      return {
+        backgroundImage: `url("${ImagePlaceholder}")`
+      }
+    }
+  }
   return (
     <Grid container className={classes.grid} spacing={2}>
       {works.map((work, index) => (
         <Grid item xs={2} key={index} className={classes.gridItem}>
-          <div style={{backgroundImage: `url("${work.img_url}")`}} className={classes.placeholderImage}>
+          <div style={imageSet(work)} className={classes.placeholderImage}>
             
             Placeholder Image
             <div className={classes.authorOverlay}>{work.title}</div>
