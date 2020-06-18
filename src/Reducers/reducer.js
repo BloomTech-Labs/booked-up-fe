eb71fb835180f806207130576123fb21d3f46bbf
Derek Glynn <48306976+wolfwood87@users.noreply.github.com>
2020-06-17 18:24:29 -0700

    Update reducer.js

--- src/Reducers/reducer.js
+++ src/Reducers/reducer.js
@@ -9,7 +9,6 @@ import { GET_USERS, SET_ADMIN } from "../actions/adminAction";
 
 import { UPLOAD_CONTENT, SET_WORK } from "../actions/authorAction";
 import { EDIT_USER, EDIT_EMAIL, DELETE_USER } from "../actions/userAction";
-import { UPLOAD_CONTENT, SET_WORK, TASK_START, TASK_FAIL, DEL_WORK } from "../actions/authorAction";
 import { UPLOAD_CONTENT, SET_WORK, TASK_START, TASK_FAIL, DEL_WORK, GET_MESSAGES } from "../actions/authorAction";
 import { SET_CONTENT, ADD_COMMENT } from "../actions/fanAction"
 
